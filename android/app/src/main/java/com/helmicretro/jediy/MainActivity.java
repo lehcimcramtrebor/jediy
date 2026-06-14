4b52369f1a1fccfca8040b9a47f31d8f16e81ad4
lehcimcramtrebor <59354081+lehcimcramtrebor@users.noreply.github.com>
2026-06-14 17:38:01 +0200

    text zoom apk

--- android/app/src/main/java/com/helmicretro/jediy/MainActivity.java
+++ android/app/src/main/java/com/helmicretro/jediy/MainActivity.java
@@ -1,5 +1,17 @@
 package com.helmicretro.jediy;
 
+import android.os.Bundle;
+import android.webkit.WebSettings;
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        
+        // Disable system font scaling (accessibility text zoom)
+        WebSettings settings = this.getBridge().getWebView().getSettings();
+        settings.setTextZoom(100);
+    }
+}
+
